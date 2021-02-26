b592a3da105cdef762eab517cbac0538dfe88ce1
Alexis Munoz <alexismun53@gmail.com>
2021-02-26 14:58:51 -0600

    Create Theme for Text

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,33 @@ class MyApp extends StatelessWidget {
       title: APP_TITLE,
       home: BeginPage(),
       theme: ThemeData(
-          primaryColor: primary,
-          secondaryHeaderColor: secondary,
-          unselectedWidgetColor: white,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ButtonStyle(
-                  textStyle:
-                      MaterialStateProperty.all(TextStyle(color: secondary)),
-                  backgroundColor: MaterialStateProperty.all(primary))),
-          inputDecorationTheme: InputDecorationTheme(
-            filled: true,
-            fillColor: Colors.white,
-            enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(0.0),
-                borderSide: BorderSide(color: white, width: 1.0)),
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(0.0),
-                borderSide: BorderSide(color: white, width: 1.0)),
-          ),
-          fontFamily: 'AkzidenzGrotesk'),
+        primaryColor: primary,
+        secondaryHeaderColor: secondary,
+        unselectedWidgetColor: white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                textStyle:
+                    MaterialStateProperty.all(TextStyle(color: secondary)),
+                backgroundColor: MaterialStateProperty.all(primary))),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(0.0),
+              borderSide: BorderSide(color: white, width: 1.0)),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(0.0),
+              borderSide: BorderSide(color: white, width: 1.0)),
+        ),
+        fontFamily: 'AkzidenzGrotesk',
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline4: TextStyle(
+              fontSize: 14, fontWeight: FontWeight.w800, color: secondary),
+          headline6: TextStyle(color: black, fontSize: 12),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
     );
   }
 }
